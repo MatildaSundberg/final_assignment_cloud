1973a7b291bbb446489940b1a68fc9a72a2d97c5
MatildaSundberg <Matildastockholm@hotmail.se>
2024-11-21 17:20:18 -0500

    created bash script to run everything

--- run_all.sh
+++ run_all.sh
@@ -1,26 +1,47 @@
 #!/bin/bash
 
 # Run main.py
-echo "Running automated script..."
+echo "Running main.py..."
 python3 main.py
 
 # Check if main.py executed successfully
 if [ $? -eq 0 ]; then
     echo "main.py executed successfully"
-    
-    # Run test.py and export results to test_results.txt
-    echo "Running test.py..."
-    python3 test.py > test_results.txt
-
-    # Check if test.py executed successfully
-    if [ $? -eq 0 ]; then
-        echo "test.py executed successfully. Results saved to test_results.txt."
-        echo "Automation is complete."
-    else
-        echo "Error: test.py failed to execute."
-        exit 1
-    fi
 else
-    echo "Error: main.py failed to execute. Skipping test.py."
+    echo "Error: main.py failed to execute. Skipping tests."
+    exit 1
+fi
+
+# Run test1.py
+echo "Running test_custom.py..."
+python3 test_custom.py
+
+if [ $? -eq 0 ]; then
+    echo "test1.py executed successfully. Results saved to test_results"
+else
+    echo "Error: test1.py failed to execute."
+    exit 1
+fi
+
+# Run test2.py
+echo "Running test_directhit.py..."
+python3 test_directhit.py
+
+if [ $? -eq 0 ]; then
+    echo "test2.py executed successfully. Results saved to test_results"
+else
+    echo "Error: test2.py failed to execute."
+    exit 1
+fi
+
+# Run test3.py
+echo "Running test_random.py..."
+python3 test_random.py
+
+if [ $? -eq 0 ]; then
+    echo "test3.py executed successfully. Results saved to test_results."
+    echo "All tests executed successfully. Automation is complete."
+else
+    echo "Error: test3.py failed to execute."
     exit 1
 fi
